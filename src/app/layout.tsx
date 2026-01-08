baa109c4051aada15f76f6e363d514601cf15c08
kumarmalla2003 <kumarmalla2003@gmail.com>
2026-01-08 18:11:13 +0530

    Fix: Use Bootstrap CSS instead of SCSS to avoid import errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/custom.scss";
 import "./globals.css";
 
